93c26ce38b26127dcc796f4679591d8dce171061
Aadarsh Mishra <aadarshakmishra16@gmail.com>
2025-05-03 12:05:54 +0530

    vercel speed analytich

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter} from "react-router-dom";
 import AppRoute from "./Component/Routing/Route";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+      <SpeedInsights/>
         <AppRoute/>
       </BrowserRouter>
     </div>
