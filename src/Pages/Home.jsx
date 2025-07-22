7441218d820860d166eebeaa3c8d2defad1a8936
Adarsh <140302686+Aadarsh6@users.noreply.github.com>
2025-07-22 22:34:24 +0530

    cleanup

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,23 +3,13 @@ import FeatureSection from "@/Component/UI/FeatureSection";
 import Footer from "@/Component/UI/Footer";
 import HeroSection from "@/Component/UI/HeroSection";
 import ProblemSection from "@/Component/UI/ProblemSection";
-import ProblemToSolutionTransition from "@/Component/UI/Sepereation";
 
 export default function HomePage() {
-  // useEffect(() => {
-  //   // Set body styles to prevent overscroll
-  //   document.body.style.backgroundColor = '#0a0a0a';
-  //   document.body.style.overscrollBehavior = 'none';
-  //   document.documentElement.style.backgroundColor = '#0a0a0a';
-  //       return () => {
-  //   };
-  // }, []);
 
   return (
     <main className="bg-[#0a0a0a] min-h-screen overflow-x-hidden">
       <HeroSection />
       <ProblemSection />
-      <ProblemToSolutionTransition/>
       <FeatureSection />
       <Footer/>
     </main>
