b9e27d55c6ac05d4231b7f50c1218d3f864a45fc
Adarsh <140302686+Aadarsh6@users.noreply.github.com>
2025-07-31 17:56:38 +0530

    deployment error2

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// import { ThemeProvider } from './Context/themeContext.jsx';
 // import { ThemeProvider, useTheme } from "./Context/index.js"
 
 createRoot(document.getElementById('root')).render(
